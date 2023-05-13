683f31368714210c5c8e18482ed27ade0c49df47
Franklin Sayre <frank.darrow@gmail.com>
2023-05-12 19:53:57 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,90 +1,115 @@
-const questionElement = document.getElementById('question');
-const choicesElement = document.getElementById('choices');
-
-const states = {
+let stages = {
     start: {
-        question: "Choose your academic path:",
-        choices: ["Undergrad", "Graduate Student", "Tenure Track Faculty"],
-        next: ["undergrad", "grad", "faculty"],
+        question: 'Welcome to the hallowed (and sometimes haunted) halls of Academia. Where do you start?',
+        choices: ['Undergrad', 'Graduate Student', 'Tenure Track Faculty']
     },
-    undergrad: {
-        question: "Late night party or late night study?",
-        choices: ["Party", "Study"],
-        next: ["dysentery", "discipline"],
+    Undergrad: {
+        question: 'Undergrad life! What will it be tonight?',
+        choices: ['Party', 'Study']
     },
-    grad: {
-        question: "Work on your thesis or take a nap?",
-        choices: ["Thesis", "Nap"],
-        next: ["discipline", "dysentery"],
+    'Graduate Student': {
+        question: 'Your thesis is due, and the allure of a nap is strong. What will you do?',
+        choices: ['Work on Thesis', 'Take a Nap']
     },
-    faculty: {
-        question: "Publish or perish?",
-        choices: ["Publish", "Perish"],
-        next: ["discipline", "dysentery"],
+    'Tenure Track Faculty': {
+        question: 'Publish or perish. What\'s your call?',
+        choices: ['Publish', 'Perish']
     },
-    discipline: {
-        question: "Choose your academic discipline:",
-        choices: ["Physics", "Biology", "Computer Science", "Fine Arts"],
-        next: ["physics", "biology", "computer_science", "fine_arts"],
+    Party: {
+        question: 'Choose your academic discipline',
+        choices: ['Physics', 'Biology', 'Computer Science', 'Fine Arts']
     },
-    physics: {
-        question: "Solve the string theory or the theory of everything?",
-        choices: ["String theory", "Theory of everything"],
-        next: ["phd", "provost"],
+    Study: {
+        question: 'Choose your academic discipline',
+        choices: ['Physics', 'Biology', 'Computer Science', 'Fine Arts']
     },
-    biology: {
-        question: "Cure cancer or discover a new species?",
-        choices: ["Cure cancer", "New species"],
-        next: ["provost", "dysentery"],
+    'Work on Thesis': {
+        question: 'Choose your academic discipline',
+        choices: ['Physics', 'Biology', 'Computer Science', 'Fine Arts']
     },
-    computer_science: {
-        question: "Build the next social media app or invent new AI?",
-        choices: ["Social media app", "New AI"],
-        next: ["startup", "phd"],
+    'Take a Nap': {
+        question: 'Choose your academic discipline',
+        choices: ['Physics', 'Biology', 'Computer Science', 'Fine Arts']
     },
-    fine_arts: {
-        question: "Paint a masterpiece or perform a groundbreaking play?",
-        choices: ["Masterpiece", "Groundbreaking play"],
-        next: ["provost", "startup"],
+    Publish: {
+        question: 'Choose your academic discipline',
+        choices: ['Physics', 'Biology', 'Computer Science', 'Fine Arts']
     },
-    dysentery: {
-        question: "You died of dysentery. Too much stress, not enough rest.",
-        choices: ["Play again?"],
-        next: ["start"],
+    Perish: {
+        question: 'You\'ve died of dysentery.',
+        choices: []
     },
-    provost: {
-        question: "Congratulations, you're appointed as Provost! Maybe now you'll have some free time?",
-        choices: ["Play again?"],
-        next: ["start"],
+    Physics: {
+        question: 'Your challenge: unravel the mysteries of the universe. What will you do?',
+        choices: ['Solve the String Theory', 'Find the Theory of Everything']
     },
-    startup: {
-        question: "You've decided to leave academia for a startup. Welcome to another kind of stress!",
-        choices: ["Play again?"],
-        next: ["start"],
+    Biology: {
+        question: 'You\'re on the brink of a breakthrough. What will you do?',
+        choices: ['Cure Cancer', 'Discover a New Species']
     },
-    phd: {
-        question: "Congratulations, you've decided to pursue another PhD. Are you a masochist?",
-        choices: ["Play again?"],
-        next: ["start"],
+    'Computer Science': {
+        question: 'The future is in your hands! What will you do?',
+        choices: ['Build the Next Social Media App', 'Invent a New AI']
     },
+    'Fine Arts': {
+        question: 'The soul of human expression! What will you do?',
+        choices: ['Paint a Masterpiece', 'Perform a Groundbreaking Play']
+    },
+    'Solve the String Theory': {
+        question: 'Congratulations, you\'ve decided to further torment yourself. You\'re getting another PhD.',
+        choices: []
+    },
+    'Find the Theory of Everything': {
+        question: 'You did it! You\'re appointed as Provost!',
+        choices: []
+    },
+    'Cure Cancer': {
+        question: 'Congratulations, you\'ve decided to further torment yourself. You\'re getting another PhD.',
+        choices: []
+    },
+    'Discover a New Species': {
+        question: 'Oh no! You\'ve died of dysentery.',
+        choices: []
+    },
+    'Build the Next Social Media App': {
+        question: 'You\'ve had enough of academia\'s rigidity and bureaucracy. You\'ve decided to work for a startup!',
+        choices: []
+    },
+    'Invent a New AI': {
+        question: 'You did it! You\'re appointed as Provost!',
+        choices: []
+    },
+    'Paint a Masterpiece': {
+        question: 'You\'ve had enough of academia\'s rigidity and bureaucracy. You\'ve decided to work for a startup!',
+        choices: []
+    },
+    'Perform a Groundbreaking Play': {
+        question: 'Congratulations, you\'ve decided to further torment yourself. You\'re getting another PhD.',
+        choices: []
+    }
 };
 
-let currentState = "start";
+let currentStage = 'start';
 
-function render() {
-    const state = states[currentState];
-    questionElement.innerText = state.question;
-    choicesElement.innerHTML = '';
-    for (let i = 0; i < state.choices.length; i++) {
-        const choice = document.createElement('div');
-        choice.className = 'choice';
-        choice.innerText = state.choices[i];
-        choice.addEventListener('click', function() {
-            currentState = state.next[i];
-            render();
+function showStage() {
+    let stage = stages[currentStage];
+
+    let questionElem = document.getElementById('question');
+    questionElem.innerText = stage.question;
+
+    let choiceContainerElem = document.getElementById('choice-container');
+    choiceContainerElem.innerHTML = '';
+
+    for (let choice of stage.choices) {
+        let buttonElem = document.createElement('button');
+        buttonElem.innerText = choice;
+        buttonElem.addEventListener('click', function() {
+            currentStage = choice;
+            showStage();
         });
-        choicesElement.appendChild(choice);
+
+        choiceContainerElem.appendChild(buttonElem);
     }
 }
 
-render();
+showStage();
